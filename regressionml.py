ea98909a6fb3bb7addaff4095ee0d51031b711e2
Mithun Parab <mithunprb@gmail.com>
2021-08-16 19:10:19 +0530

    r-square

--- regressionml.py
+++ regressionml.py
@@ -2,31 +2,38 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+style.use('ggplot')
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+xs = np.array([1,2,3,4,5], dtype=np.float64)
+ys = np.array([5,4,6,5,6], dtype=np.float64)
 
-def best_fit_slope_and_intercept(x, y):
-	# m = (Ẍ*Ȳ - mean(ẌȲ))/(Ẍ^2 - (Ẍ)^2)
-	m = ( 
-		( mean(x) * mean(y) - mean(x * y) )/
-		( mean(x)*mean(x) - (mean(x*x)) ) 
-		)
-	b = mean(y) - m*(mean(x))
-	return m, b
+def best_fit_slope_and_intercept(xs, ys):
+    m = (
+    	(  (mean(xs)*mean(ys)) - mean(xs*ys)) /
+           ((mean(xs)*mean(xs)) - mean(xs*xs)  )
+         )
+    b = mean(ys) - m*mean(xs)
 
-m, b =best_fit_slope_and_intercept(xs, ys)
-#b = y - mx
+    return m, b
 
-print(m, b)
+def squared_error(ys_orig, ys_line):
 
-regression_line = [(m*x)+ b for x in xs]
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
 
-predict_x = 8
-predict_y = (m*predict_x) + b
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    
+    return 1 - (squared_error_regr/squared_error_y_mean)
+    
+m, b = best_fit_slope_and_intercept(xs,ys)
+regression_line = [(m*x) + b for x in xs]
 
+r_squared = coefficient_of_determination(ys,regression_line)
+print(r_squared)
 
-plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y, color='g')
-plt.plot(xs, regression_line)
-plt.show()
+##plt.scatter(xs,ys,color='#003F72',label='data')
+##plt.plot(xs, regression_line, label='regression line')
+##plt.legend(loc=4)
+##plt.show()
