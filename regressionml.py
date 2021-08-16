6f79a67df9054f329ed2008c6f235bcb5fd7042f
Mithun Parab <mithunprb@gmail.com>
2021-08-16 16:20:11 +0530

    pickling classifer

--- regressionml.py
+++ regressionml.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
+
 
 style.use('ggplot')
 
@@ -42,19 +44,23 @@ x_lately = x[-forecast_out:]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-y = np.array(df['label'])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 #clf = svm.SVR()
 
 #clf = svm.SVR(kernel='poly')
-clf = LinearRegression(n_jobs=-1)
+# clf = LinearRegression(n_jobs=-1)
+# clf.fit(x_train, y_train)
 
-clf.fit(x_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+# 	pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(x_test, y_test)
 #print(accuracy)
-
 forecast_set = clf.predict(x_lately)
 
 print(forecast_set, accuracy, forecast_out)
